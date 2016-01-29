112f96ab9f5a4d49a832f7c84943de537714a027
Roberto Ferreira Junior <robeferre@gmail.com>
2016-01-29 14:33:37 -0200

    Initial Release

---
+++ settings.py
@@ -0,0 +1,18 @@
+ENVIRONMENTS = {
+    "streaming": {
+        "real": "stream-fxtrade.oanda.com",
+        "practice": "stream-fxpractice.oanda.com",
+        "sandbox": "stream-sandbox.oanda.com"
+    },
+    "api": {
+        "real": "api-fxtrade.oanda.com",
+        "practice": "api-fxpractice.oanda.com",
+        "sandbox": "api-sandbox.oanda.com"
+    }
+}
+
+DOMAIN = "practice"
+STREAM_DOMAIN = ENVIRONMENTS["streaming"][DOMAIN]
+API_DOMAIN = ENVIRONMENTS["api"][DOMAIN]
+ACCESS_TOKEN = 'b54c8dc39264bffd7d7016ee231145f1-83bd0c367f886b8808e664874c2430ad'
+ACCOUNT_ID =  4617133
